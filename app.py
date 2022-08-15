c1276a2a8ab8935bd60a2ed86c49117a981b9d5e
BryteLitty <littyofficials@gmail.com>
2022-08-15 23:22:03 +0000

    modules created

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 
+from email.policy import default
 import json
 from pickletools import markobject
 import dateutil.parser
@@ -57,6 +58,21 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+# Venue Model
+class Venue(db.Model):
+      __tablename__="Venues"
+      id = db.Column(db.Integer, primary_key=True)
+      name = db.Column(db.String(120))
+      city = db.Column(db.String(120))
+      state = db.Column(db.String(120))
+      phone = db.Column(db.String(120))
+      genres = db.Column(db.String(120))  
+      facebook_link = db.Column(db.String(120))
+      image_link = db.Column(db.String(120))
+      website_link = db.Column(db.String(120))
+      looking_for_talent = db.Column(db.Boolean, default=False)
+      
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
--- config.py
+++ config.py
@@ -10,6 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:5432/fyyur"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@localhost:5432/todoapp"
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
